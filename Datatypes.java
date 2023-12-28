f72a11a935802be6495b947724658f213be375b2
Vishnushankar1897 <155039802+Vishnushankar1897@users.noreply.github.com>
2023-12-28 09:10:23 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,22 @@
+class Datatypes
+
+{
+public static void main (String[] args)
+	{
+	boolean b=true;
+	int i=10;
+	char c='V';
+	byte w=2;
+	short s=5;
+	long l=123345L;
+	float f=42.35f;
+	System.out.println("Boolean: "+b);
+	System.out.println(i);	
+	System.out.println(c);
+	System.out.println(w);
+	System.out.println(s);	
+	System.out.println(l);
+	System.out.println(f);	
+}
+
+}
---
+++ Variables.java
@@ -0,0 +1,14 @@
+class Variables
+{
+	static String course= "Java";
+	static String section="XI";
+	public static void main(String[] args)
+
+	{
+	String studentName;
+	studentName="Vishnu Shankar";
+	System.out.println(studentName);
+	System.out.println(course);
+	System.out.println(section);
+	}
+}
